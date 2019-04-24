a5adf8cbe1933e97aad1dbba9a90e8e529e7da6f
mahonggang <1584157651@qq.com>
2019-04-24 20:58:46 +0800

    ss

---
+++ src/main/java/com/ffw/web/controller/BillController.java
@@ -0,0 +1,43 @@
+package com.ffw.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ffw.api.model.Page;
+import com.ffw.api.model.PageData;
+import com.ffw.web.constant.IConstant;
+import com.ffw.web.util.RestTemplateUtil;
+
+@Controller
+@RequestMapping("bill")
+public class BillController extends BaseController {
+
+	private Logger logger = LoggerFactory.getLogger(BillController.class);
+
+	@Autowired
+	RestTemplateUtil rest;
+
+	@RequestMapping("listPage")
+	public ModelAndView listPage() throws Exception {
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		String keywords = pd.getString("keywords"); // 检索条件
+		if (null != keywords && !"".equals(keywords)) {
+			pd.put("keywords", keywords.trim());
+		}
+
+		Page page = rest.post(IConstant.FFW_SERVICE_KEY, "bill/listPage", pd,
+				Page.class);
+
+		mv.setViewName("/bill/list");
+		mv.addObject("page", page);
+		mv.addObject("pd", pd);
+		logger.info("分页查询账单信息");
+		return mv;
+	}
+}
